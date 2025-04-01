1d77bbb8fededc33a061bfd05c630a7dca6ef0b6
ServicePlace <lafitnesssmm@gmail.com>
2025-04-01 18:04:19 +0000

    temp env info

--- src/payment-system/config.ts
+++ src/payment-system/config.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from '@solana/web3.js';
 
-export const STORE_WALLET = new PublicKey('YOUR_STORE_WALLET_ADDRESS');
-export const PROCESSOR_WALLET = new PublicKey('YOUR_PROCESSOR_WALLET_ADDRESS');
-export const ACCEPTED_TOKEN_MINT = new PublicKey('YOUR_HALOTECH_COIN_MINT_ADDRESS');
+export const STORE_WALLET = new PublicKey('DfMjbyWr32RThxtdgYH7CwnV73XPT8PQQmqfpLL7QYpP');
+export const PROCESSOR_WALLET = new PublicKey('H1o49AxeG9CcezMhjsWhxsvDCYMh21PnUYPn8oy27ecf');
+export const ACCEPTED_TOKEN_MINT = new PublicKey('mntEiNGoTrqjkL1BSi18fwgssboGa3pLX2VQg74cCC3');
 export const SOLANA_NETWORK = 'https://api.devnet.solana.com'; // Use mainnet for production
 export const PROCESSING_FEE = 0.007; // Fee in SOL
